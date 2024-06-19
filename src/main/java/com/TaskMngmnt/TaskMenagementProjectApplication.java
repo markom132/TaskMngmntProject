59d9fc059a334ccd95d1a0322efb44719452cfa0
Marko Markovic <markomarkoviccb@gmail.com>
2024-06-19 19:49:22 +0200

    first commit

---
+++ src/main/java/com/TaskMngmnt/TaskMenagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.TaskMngmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMenagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMenagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TaskMngmnt/TaskMenagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskMngmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMenagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
